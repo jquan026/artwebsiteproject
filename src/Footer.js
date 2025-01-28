848d273c17af87d6710e24401530e1dbfa48446c
Jenny Quan <toxic_dreamer@MacBook-Air-8.attlocal.net>
2025-01-27 23:33:22 -0800

    added social media links

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { BsInstagram } from "react-icons/bs";
 import { AiOutlineFacebook } from "react-icons/ai";
+import { FaFacebookSquare } from "react-icons/fa";
 import './Footer.css'
 import FAQ from "./FAQ"
 import paintbrush from "./img/paintbrush.png";
@@ -13,8 +14,22 @@ const Footer = () => {
       <div className="footer">
         <div className="socials-icons">
           <p className="gallery-text">SOCIAL MEDIA GALLERY: </p>
-          <BsInstagram className="socials" />
-          <AiOutlineFacebook className="socials" />
+          <a
+            href="https://www.instagram.com/artwithlarisse/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socials"
+          >
+            <BsInstagram />
+          </a>
+          <a
+            href="https://www.facebook.com/artwithlarisse/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="socials"
+          >
+            <FaFacebookSquare />
+          </a>
         </div>
         <div className="links">
           <Link to="/faq" className="footer-q">
