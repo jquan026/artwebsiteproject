da37d04b1e11f28026837897d0f63848eb2b4884
Jenny Quan <toxic_dreamer@MacBook-Air-8.attlocal.net>
2025-01-27 22:19:36 -0800

    route FAQ to footer FAQ

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { BsInstagram } from "react-icons/bs";
 import { AiOutlineFacebook } from "react-icons/ai";
 import './Footer.css'
+import FAQ from "./FAQ"
 import paintbrush from "./img/paintbrush.png";
 
 const Footer = () => {
@@ -11,17 +13,18 @@ const Footer = () => {
       <div className="footer">
         <div className="socials-icons">
           <p className="gallery-text">SOCIAL MEDIA GALLERY: </p>
-          <BsInstagram className="socials"/>
-          <AiOutlineFacebook className="socials"/>
+          <BsInstagram className="socials" />
+          <AiOutlineFacebook className="socials" />
         </div>
         <div className="links">
-          <p className="footer-q">FREQUENTLY ASKED QUESTIONS</p>
+          <Link to="/faq" className="footer-q">
+            FREQUENTLY ASKED QUESTIONS
+          </Link>
         </div>
-        <div className="paintbrush-img">
-        </div>
+        <div className="paintbrush-img"></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer
