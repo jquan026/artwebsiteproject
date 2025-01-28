b1c3fbac671d646f21f83964c1114caf00fcf7a1
Jenny Quan <toxic_dreamer@MacBook-Air-8.attlocal.net>
2025-01-27 22:21:31 -0800

    added routing to FAQ

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Navbar from './Navbar';
 import Logo from "./Logo"
 import Homepage from './Homepage';
 import Homepage2 from './Homepage2';
+import FAQ from './FAQ'
+import Team from "./Team"
 function App() {
   return (
     <>
       <Logo classname="logo"/>
       <Navbar classname="navbar"/>
-      <Homepage2 classname="homepage"/>
       <Routes>
         <Route path="/" element={<Homepage2 />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/instructors" element={<Team />} />
       </Routes>
     </>
   );
