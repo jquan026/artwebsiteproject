d144f434fc4c4ae493fcfa07a307254768083157
Jenny Quan <toxic_dreamer@MacBook-Air-8.lan>
2025-02-08 00:33:05 -0800

    added birthday parties page

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Homepage2 from './Homepage2';
 import FAQ from './FAQ';
 import Team from "./Team";
 import StudioPolicies from './StudioPolicies';
+import BirthdayParties from './BirthdayParties'
 
 function App() {
   return (
     <>
-      <Logo classname="logo"/>
-      <Navbar classname="navbar"/>
+      <Logo classname="logo" />
+      <Navbar classname="navbar" />
       <Routes>
         <Route path="/" element={<Homepage2 />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/instructors" element={<Team />} />
         <Route path="/studio-policies" element={<StudioPolicies />} />
+        <Route path="/birthday-parties" element={<BirthdayParties />} />
       </Routes>
     </>
   );
